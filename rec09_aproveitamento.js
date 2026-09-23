833471dd407c53e7ca1869aeeaa01443aa2a3610
Lillyyvt <lv212474@gmail.com>
2026-09-23 11:25:47 -0300

    ex09

--- rec09_aproveitamento.js
+++ rec09_aproveitamento.js
@@ -10,4 +10,26 @@
 
 const entrada = require('readline-sync');
 
-function calcularpercentual()
+const total = entrada.questionInt("Digite a quantidade total: ");
+const util = entrada.questionInt("Digite a quantidade util: ");
+
+function calcularAproveitamento(util, total) {
+    return (util / total) * 100;
+}
+
+function classificarAproveitamento(percentual) {
+    if (percentual >= 90) {
+        return "EXCELENTE";
+    } else if (percentual >= 75) {
+        return "ADEQUADO";
+    } else {
+        return "REVISAR PROCESSO";
+    }
+}
+
+const percentual = calcularAproveitamento(util, total);
+const classificacao = classificarAproveitamento(percentual);
+
+console.log(`Percentual de aproveitamento: ${percentual.toFixed(2)}%`);
+console.log(`Classificação: ${classificacao}`);
+console.log("-".repeat(30));
